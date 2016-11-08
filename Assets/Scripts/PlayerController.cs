c95635cc27f501fcadc18ae2643801f6080b0119
Tyler Marraffa <Tyler Marraffa>
2016-11-08 18:31:51 -0500

    Movement and Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,45 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
+
+	public float maxSpeed;
+
+	Rigidbody2D myRB;
+	Animator myAnim;
+	bool facingRight;
+
 	void Start () {
-	
+		myRB = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+
+		facingRight = true;
 	}
-	
+
+
+	void FixedUpdate () {
+		//Quaternion standUp = transform.localRotation;
+		//standUp.y = 0;
+		//transform.localRotation = standUp;
+
+		float move = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("Speed", Mathf.Abs (move));
+
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight) {
+			flip ();
+		}
+	}
+
+	void flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
