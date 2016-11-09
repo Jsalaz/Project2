7755b89c127b647e4ed1d0ad2163fe83a812912e
Tyler Marraffa <Tyler Marraffa>
2016-11-08 23:12:29 -0500

    Jumping, Gravity, and complete Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
-
+	//movement variables
 	public float maxSpeed;
+
+	//jumping variables
+	bool grounded = false;
+	float groundCheckRadius = 0.2f;
+	public LayerMask groundLayer;
+	public Transform groundCheck;
+	public float jumpHeight;
+
 
 	Rigidbody2D myRB;
 	Animator myAnim;
@@ -14,19 +22,30 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		myRB = GetComponent<Rigidbody2D> ();
 		myAnim = GetComponent<Animator> ();
-
 		facingRight = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (grounded && Input.GetAxis ("Jump") > 0) {
+			grounded = false;
+			myAnim.SetBool ("IsGrounded", grounded);
+			myRB.AddForce (new Vector2 (0, jumpHeight));
+		}
 	}
 
 
 	void FixedUpdate () {
-		//Quaternion standUp = transform.localRotation;
-		//standUp.y = 0;
-		//transform.localRotation = standUp;
+
+		//check if we are grounded, if not, then we are falling
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+		myAnim.SetBool ("IsGrounded", grounded);
+
+		myAnim.SetFloat ("VerticalSpeed", myRB.velocity.y);
+
 
 		float move = Input.GetAxis ("Horizontal");
 		myAnim.SetFloat ("Speed", Mathf.Abs (move));
-
 		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
 
 		if (move > 0 && !facingRight) {
@@ -36,6 +55,7 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+
 	void flip() {
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
@@ -43,8 +63,5 @@ public class PlayerController : MonoBehaviour {
 		transform.localScale = theScale;
 	}
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
