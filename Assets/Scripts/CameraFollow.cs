3c62f7f1eb8560584fcd0797870cf0ffc0476c95
Piotr Romanowski <piotrromanowski1@gmail.com>
2016-11-10 01:06:06 -0500

    added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	#region private serialized variables
+	[SerializeField]
+	private Vector3 followOffset;	//the follow offset of the camera
+	[SerializeField]
+	private float smoothFollowAmount = 0f;
+	[SerializeField]
+	private string followTag;
+	#endregion
+
+	#region private variables
+	private Transform meCamera;		//the camera it self
+	private Transform targetObject;	//the object to follow
+	#endregion
+
+	void Awake(){
+		meCamera = this.transform;	//assign itself to variable
+		targetObject = GameObject.FindWithTag(followTag).transform;		//get and assign the player object
+	}
+
+	void LateUpdate(){
+		meCamera.position = new Vector3(Mathf.SmoothStep(meCamera.position.x, targetObject.position.x + followOffset.x, Time.deltaTime * smoothFollowAmount),
+                                        Mathf.SmoothStep(meCamera.position.y, targetObject.position.y + followOffset.y, Time.deltaTime * smoothFollowAmount) ,
+                                        Mathf.SmoothStep(meCamera.position.z, targetObject.position.z + followOffset.z, Time.deltaTime * smoothFollowAmount));		//follow the player
+	}
+}
